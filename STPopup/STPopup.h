bee461bd35fb41e4ae90329b764a86572dd20b4d
Kevin <kevin_lyn@outlook.com>
2015-09-13 22:52:13 +0800

    Add public header file.

---
+++ STPopup/STPopup.h
@@ -0,0 +1,14 @@
+//
+//  STPopup.h
+//  Sth4Me
+//
+//  Created by Kevin Lin on 13/9/15.
+//  Copyright (c) 2015 Sth4Me. All rights reserved.
+//
+
+#ifndef STPopupExample_STPopup_h
+#define STPopupExample_STPopup_h
+
+#import <STPopup/STPopupController.h>
+
+#endif
