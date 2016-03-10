80dd79f1ee28386ff0000e2b8c64c21f97fcee1b
Stefano Acerbetti <sacerbetti@birst.com>
2016-03-10 15:59:16 -0800

    Pass the touch event of the custom navigation bar also if the draggable property is set to true

--- STPopup/STPopupNavigationBar.m
+++ STPopup/STPopupNavigationBar.m
@@ -37,10 +37,7 @@
 
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
 {
-    if (!self.draggable) {
-        [super touchesBegan:touches withEvent:event];
-        return;
-    }
+    [super touchesBegan:touches withEvent:event];
     
     UITouch *touch = [touches anyObject];
     if ((touch.view == self || touch.view.superview == self) && !_moving) {
@@ -51,10 +48,7 @@
 
 - (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
 {
-    if (!self.draggable) {
-        [super touchesMoved:touches withEvent:event];
-        return;
-    }
+    [super touchesMoved:touches withEvent:event];
     
     if (_moving) {
         UITouch *touch = [touches anyObject];
@@ -67,30 +61,24 @@
 
 - (void)touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event
 {
-    if (!self.draggable) {
-        [super touchesCancelled:touches withEvent:event];
-        return;
-    }
-    
     if (_moving) {
         UITouch *touch = [touches anyObject];
         float offset = [touch locationInView:self.window].y - _movingStartY;
         [self movingDidEndWithOffset:offset];
     }
+    
+    [super touchesCancelled:touches withEvent:event];
 }
 
 - (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
 {
-    if (!self.draggable) {
-        [super touchesEnded:touches withEvent:event];
-        return;
-    }
-    
     if (_moving) {
         UITouch *touch = [touches anyObject];
         float offset = [touch locationInView:self.window].y - _movingStartY;
         [self movingDidEndWithOffset:offset];
     }
+    
+    [super touchesEnded:touches withEvent:event];
 }
 
 - (void)movingDidEndWithOffset:(float)offset
