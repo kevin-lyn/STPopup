a2068a5982c71b467d5abeabed613e94e7285833
zhongwuzw <zhongwuzw@qq.com>
2017-08-29 17:48:28 +0800

    fix navigationBar touch event related issue

--- STPopup/STPopupNavigationBar.m
+++ STPopup/STPopupNavigationBar.m
@@ -101,4 +101,22 @@
     }
 }
 
+- (UIView *)hitTest:(CGPoint)point withEvent:(UIEvent *)event{
+    if (!self.isUserInteractionEnabled || self.isHidden || self.alpha <= 0.01) {
+        return nil;
+    }
+    
+    if ([self pointInside:point withEvent:event]) {
+        for (UIView *subview in [self.subviews reverseObjectEnumerator]) {
+            CGPoint convertedPoint = [subview convertPoint:point fromView:self];
+            UIView *hitTestView = [subview hitTest:convertedPoint withEvent:event];
+            if (hitTestView) {
+                return hitTestView;
+            }
+        }
+        return self;
+    }
+    return nil;
+}
+
 @end
