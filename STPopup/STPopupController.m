8122f6e61d9b1b4ab6095b5a3e24d48447a8d544
Kevin <kevin_lyn@outlook.com>
2018-05-04 00:47:33 +0100

    Update to recommended project settings.

--- STPopup/STPopupController.m
+++ STPopup/STPopupController.m
@@ -379,16 +379,16 @@ static NSMutableSet *_retainedPopupControllers;
         toViewController.view.alpha = 0;
         [UIView animateWithDuration:0.5 delay:0 usingSpringWithDamping:1 initialSpringVelocity:0 options:UIViewAnimationOptionCurveEaseInOut animations:^{
             [self layoutContainerView];
-            [_contentView addSubview:toViewController.view];
+            [self->_contentView addSubview:toViewController.view];
             capturedView.alpha = 0;
             toViewController.view.alpha = 1;
-            [_containerViewController setNeedsStatusBarAppearanceUpdate];
+            [self->_containerViewController setNeedsStatusBarAppearanceUpdate];
         } completion:^(BOOL finished) {
             [capturedView removeFromSuperview];
             [fromViewController removeFromParentViewController];
             
-            _containerView.userInteractionEnabled = YES;
-            [toViewController didMoveToParentViewController:_containerViewController];
+            self->_containerView.userInteractionEnabled = YES;
+            [toViewController didMoveToParentViewController:self->_containerViewController];
             
             [fromViewController endAppearanceTransition];
             [toViewController endAppearanceTransition];
@@ -501,10 +501,10 @@ static NSMutableSet *_retainedPopupControllers;
         _navigationBar.hidden = navigationBarHidden;
     }
     [UIView animateWithDuration:0.5 delay:0 usingSpringWithDamping:1 initialSpringVelocity:0 options:UIViewAnimationOptionCurveEaseInOut animations:^{
-        _navigationBar.alpha = navigationBarHidden ? 0 : 1;
+        self->_navigationBar.alpha = navigationBarHidden ? 0 : 1;
         [self layoutContainerView];
     } completion:^(BOOL finished) {
-        _navigationBar.hidden = navigationBarHidden;
+        self->_navigationBar.hidden = navigationBarHidden;
     }];
 }
 
@@ -649,11 +649,11 @@ static NSMutableSet *_retainedPopupControllers;
 {
     [_containerView endEditing:YES];
     [UIView animateWithDuration:0.2 delay:0 usingSpringWithDamping:1 initialSpringVelocity:0 options:UIViewAnimationOptionCurveEaseInOut animations:^{
-        _containerView.alpha = 0;
+        self->_containerView.alpha = 0;
     } completion:^(BOOL finished) {
         [self layoutContainerView];
         [UIView animateWithDuration:0.2 delay:0 usingSpringWithDamping:1 initialSpringVelocity:0 options:UIViewAnimationOptionCurveEaseInOut animations:^{
-            _containerView.alpha = 1;
+            self->_containerView.alpha = 1;
         } completion:nil];
     }];
 }
@@ -860,12 +860,12 @@ static NSMutableSet *_retainedPopupControllers;
         _containerView.transform = CGAffineTransformIdentity;
         
         [UIView animateWithDuration:[transitioning popupControllerTransitionDuration:context] delay:0 usingSpringWithDamping:1 initialSpringVelocity:1 options:UIViewAnimationOptionCurveEaseInOut animations:^{
-            _backgroundView.alpha = lastBackgroundViewAlpha;
+            self->_backgroundView.alpha = lastBackgroundViewAlpha;
         } completion:nil];
         
         [transitioning popupControllerAnimateTransition:context completion:^{
-            _backgroundView.userInteractionEnabled = YES;
-            _containerView.userInteractionEnabled = YES;
+            self->_backgroundView.userInteractionEnabled = YES;
+            self->_containerView.userInteractionEnabled = YES;
             
             [transitionContext completeTransition:![transitionContext transitionWasCancelled]];
             [topViewController didMoveToParentViewController:toViewController];
@@ -883,12 +883,12 @@ static NSMutableSet *_retainedPopupControllers;
         _containerView.userInteractionEnabled = NO;
         
         [UIView animateWithDuration:[transitioning popupControllerTransitionDuration:context] delay:0 options:UIViewAnimationOptionCurveEaseOut animations:^{
-            _backgroundView.alpha = 0;
+            self->_backgroundView.alpha = 0;
         } completion:nil];
         
         [transitioning popupControllerAnimateTransition:context completion:^{
-            _backgroundView.userInteractionEnabled = YES;
-            _containerView.userInteractionEnabled = YES;;
+            self->_backgroundView.userInteractionEnabled = YES;
+            self->_containerView.userInteractionEnabled = YES;;
             
             [fromViewController.view removeFromSuperview];
             [transitionContext completeTransition:![transitionContext transitionWasCancelled]];
@@ -898,7 +898,7 @@ static NSMutableSet *_retainedPopupControllers;
             
             [toViewController endAppearanceTransition];
             
-            _backgroundView.alpha = lastBackgroundViewAlpha;
+            self->_backgroundView.alpha = lastBackgroundViewAlpha;
         }];
     }
 }
@@ -927,7 +927,7 @@ static NSMutableSet *_retainedPopupControllers;
     else {
         [_containerView endEditing:YES];
         [UIView animateWithDuration:0.4 delay:0 usingSpringWithDamping:0.7 initialSpringVelocity:0 options:UIViewAnimationOptionCurveEaseInOut animations:^{
-            _containerView.transform = CGAffineTransformIdentity;
+            self->_containerView.transform = CGAffineTransformIdentity;
         } completion:nil];
     }
 }
