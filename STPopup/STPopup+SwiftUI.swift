65388aba207281ece978ba8fa6ca071b2b43d160
Kevin <kevin_lyn@outlook.com>
2021-12-11 20:49:31 +0000

    Create PopupView only when it's presented.

--- STPopup/STPopup+SwiftUI.swift
+++ STPopup/STPopup+SwiftUI.swift
@@ -14,23 +14,25 @@ public typealias PopupStyle = STPopupStyle
 
 @available (iOS 13.0, *)
 public extension View {
-  func popup<ContentView>(
+  @ViewBuilder func popup<ContentView>(
     isPresented: Binding<Bool>,
     style: PopupStyle = .formSheet,
     onDismiss: (() -> Void)? = nil,
     @ViewBuilder contentView: @escaping () -> ContentView
   ) -> some View where ContentView: View {
-    overlay(
-      // We don't need a binding for now. It's exposed as `Binding` so that we can change internal implementation
-      // without affecting public API.
-      PopupView(isPresented: isPresented.wrappedValue, style: style, onDismiss: onDismiss, contentView: contentView())
-        .frame(
-          width: isPresented.wrappedValue ? Self.windowSize.width : 0,
-          height: isPresented.wrappedValue ? Self.windowSize.height : 0
-        )
-    )
+    if isPresented.wrappedValue {
+      overlay(
+        PopupView(isPresented: isPresented, style: style, onDismiss: onDismiss, contentView: contentView())
+          .frame(
+            width: isPresented.wrappedValue ? Self.windowSize.width : 0,
+            height: isPresented.wrappedValue ? Self.windowSize.height : 0
+          )
+      )
+    } else {
+      self
+    }
   }
-  
+
   // Window size is needed to properly layout content view when `sizeThatFits` is called.
   private static var windowSize: CGSize {
     if let scene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
@@ -44,7 +46,7 @@ public extension View {
 
 @available (iOS 13.0, *)
 fileprivate struct PopupView<ContentView>: UIViewControllerRepresentable where ContentView: View {
-  let isPresented: Bool
+  @Binding var isPresented: Bool
   let style: PopupStyle
   let onDismiss: (() -> Void)?
   let contentView: ContentView
