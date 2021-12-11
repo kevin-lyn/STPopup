8c5e7c184bdfae48995a04f758dd8c55ec9b02a9
Kevin <kevin_lyn@outlook.com>
2021-12-11 21:09:11 +0000

    SwiftUI: example for bottom sheet.

--- STPopupSwiftUIExample/STPopupSwiftUIExample/ContentView.swift
+++ STPopupSwiftUIExample/STPopupSwiftUIExample/ContentView.swift
@@ -9,37 +9,74 @@ import SwiftUI
 import STPopup
 
 struct ContentView: View {
-  @State private var isPresentingPopup = false
+  @State private var isPresentingFormSheetPopup = false
+  @State private var isPresentingBottomSheetPopup = false
   @State private var showsAdditionalMessage = false
   
   var body: some View {
-    Button("Show Popup") {
-      isPresentingPopup = true
+    NavigationView {
+      List {
+        Button("Show Popup(style = .formSheet)") {
+          isPresentingFormSheetPopup = true
+        }
+        Button("Show Popup(style = .bottomSheet)") {
+          isPresentingBottomSheetPopup = true
+        }
+      }
+      .listStyle(.plain)
+      .popup(isPresented: $isPresentingFormSheetPopup) {
+        popupContent {
+          isPresentingFormSheetPopup = false
+        }
+        .padding(.leading)
+        .padding(.trailing)
+        .background(Color.white)
+        .cornerRadius(10)
+      }
+      .popup(isPresented: $isPresentingBottomSheetPopup, style: .bottomSheet) {
+        VStack {
+          HStack {
+            Spacer()
+          }
+          popupContent {
+            isPresentingBottomSheetPopup = false
+          }
+        }
+        .background(
+          VStack {
+            HStack {
+              Spacer()
+            }
+            Spacer()
+          }
+          .background(Color.white)
+          .cornerRadius(10)
+          .padding(.leading)
+          .padding(.trailing)
+        )
+      }
+      .navigationTitle("STPopup+SwiftUI")
+      .onAppear {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+          showsAdditionalMessage = true
+        }
+      }
+    }
+  }
+
+  private func popupContent(action: @escaping () -> Void) -> some View {
+    VStack(spacing: 10) {
+      Image(systemName: "location")
+        .font(.system(.largeTitle))
+      Text("Location Service")
+      if showsAdditionalMessage {
+        Text("Tap to dismiss")
+      }
+      Button("Enable") {
+        action()
+      }
     }
     .padding()
-    .popup(isPresented: $isPresentingPopup) {
-      VStack(spacing: 10) {
-        Image(systemName: "location")
-          .font(.system(.largeTitle))
-        Text("Location Service")
-        if showsAdditionalMessage {
-          Text("Tap to dismiss")
-        }
-        Button("Enable") {
-          isPresentingPopup = false
-        }
-      }
-      .padding()
-      .padding(.leading)
-      .padding(.trailing)
-      .background(Color(.white))
-      .cornerRadius(10)
-    }
-    .onAppear {
-      DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
-        showsAdditionalMessage = true
-      }
-    }
   }
 }
 
